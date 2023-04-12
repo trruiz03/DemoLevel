aea503fa499b308c7535d78d05125573e5f528ea
Thomas Ruiz <thomasrruiz03@gmail.com>
2023-04-12 10:47:26 -0600

    updating Unity to 2022

---
+++ IndividualDemoProject/Assets/Scripts/AdaptiveAudio/AdaptiveAudioManager.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Audio;
+
+public class AdaptiveAudioManager : Singleton<AdaptiveAudioManager> {
+
+    public int currentAdaptiveLevel;
+    public AudioMixerSnapshot[] snapshotLevels;
+    public float transitionTime = 2; // This is public so set it in the inspector
+
+	public void AdjustAudioLevel(int level)
+    {
+        currentAdaptiveLevel = level;
+
+        snapshotLevels[currentAdaptiveLevel-1].TransitionTo(transitionTime);
+    }
+}
