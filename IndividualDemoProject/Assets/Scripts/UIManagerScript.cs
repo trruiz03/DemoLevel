c7dce3c9bb26ededcb4adc10ea2a3d9e921c59b9
Thomas Ruiz <94023824+trruiz03@users.noreply.github.com>
2023-12-15 23:47:45 -0700

    Finished
    Added final hold UI panel for in game

--- IndividualDemoProject/Assets/Scripts/UIManagerScript.cs
+++ IndividualDemoProject/Assets/Scripts/UIManagerScript.cs
@@ -35,6 +35,7 @@ public class UIManagerScript : MonoBehaviour
         gameTitle.gameObject.SetActive(false);
         startButton.gameObject.SetActive(false);
         detailButton.gameObject.SetActive(false);
+        controlButton.gameObject.SetActive(false);
         detailPage.gameObject.SetActive(true);
         backButton.gameObject.SetActive(true);
     }
@@ -43,9 +44,11 @@ public class UIManagerScript : MonoBehaviour
     {
         detailPage.gameObject.SetActive(false);
         backButton.gameObject.SetActive(false);
+        controlPage.gameObject.SetActive(false); ;
         gameTitle.gameObject.SetActive(true);
         startButton.gameObject.SetActive(true);
         detailButton.gameObject.SetActive(true);
+        controlButton.gameObject.SetActive(true);
     }
 
     public void Controls()
@@ -53,6 +56,7 @@ public class UIManagerScript : MonoBehaviour
         gameTitle.gameObject.SetActive(false);
         startButton.gameObject.SetActive(false);
         detailButton.gameObject.SetActive(false);
+        controlButton.gameObject.SetActive(false); 
         detailPage.gameObject.SetActive(false);
         controlPage.gameObject.SetActive(true);
         backButton.gameObject.SetActive(true);
